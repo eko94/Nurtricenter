58fe57e40c48740e21788bd7fe1f4c76af5c1a3d
Eiji <eijikondo_94@hotmail.com>
2025-06-21 00:06:58 -0400

    Corrige saltos de línea de CRLF a LF. Corrige los puertos. Agrega el servicio nutricionista en Service Discovery

--- .consul/consul-init.sh
+++ .consul/consul-init.sh
@@ -5,3 +5,5 @@ consul services register -name=identity -address=identity -port=8080
 consul services register -name=catering -address=catering -port=8080
 
 consul services register -name=commercial -address=commercial-api -port=80
+
+consul services register -name=nutricionista -address=nutricionista -port=8080
