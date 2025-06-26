ac36b1861499b1b95f7e1bf7f1c80525d70a5f53
Eiji <eijikondo_94@hotmail.com>
2025-06-25 22:51:38 -0400

    Agrega http en los address de consul

--- .consul/consul-init.sh
+++ .consul/consul-init.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-consul services register -name=identity -address=identity -port=8080
+consul services register -name=identity -address=http://identity -port=8080
 
-consul services register -name=catering -address=catering -port=8080
+consul services register -name=catering -address=http://catering -port=8080
 
-consul services register -name=commercial -address=commercial-api -port=80
+consul services register -name=commercial -address=http://commercial-api -port=80
 
-consul services register -name=nutricionista -address=nutricionista -port=8080
+consul services register -name=nutricionista -address=http://nutricionista -port=8080
