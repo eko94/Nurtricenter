a3b15a730963ade37e94d23c3e8f4318fcb76733
Eiji <eijikondo_94@hotmail.com>
2025-07-04 18:42:21 -0400

    Agrega nuevo servicio en Consul

--- .consul/consul-init.sh
+++ .consul/consul-init.sh
@@ -7,3 +7,5 @@ consul services register -name=catering -address=http://catering -port=8080
 consul services register -name=commercial -address=http://commercial-api -port=80
 
 consul services register -name=nutricionista -address=http://nutricionista -port=8080
+
+consul services register -name=logistica -address=http://logistica -port=8080
